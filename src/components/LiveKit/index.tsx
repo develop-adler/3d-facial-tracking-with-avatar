beeb41cead13fc9d177b437a986cc68e5a849ce2
gavinadler <giavi.quach@adler.cx>
2025-05-10 14:16:31 +0700

    sanitize inputs

--- src/components/LiveKit/index.tsx
+++ src/components/LiveKit/index.tsx
@@ -3,6 +3,8 @@
 import dynamic from "next/dynamic";
 import { type FC } from "react";
 
+import DOMPurify from 'dompurify';
+
 import type { RoomAndName } from "@/models/multiplayer";
 import { JoinRoomModal } from "@/components/LiveKit/JoinRoomModal";
 import { useLiveKitStore } from "@/stores/useLiveKitStore";
@@ -19,6 +21,19 @@ export const LiveKitPage: FC = () => {
     const setRoomNameAndUsername = useLiveKitStore((state) => state.setRoomNameAndUsername);
 
     const handleFormSubmit = (data: RoomAndName) => {
+        // Define the regex to allow only a-z, A-Z, 0-9
+        const regex = /^[\dA-Za-z]*$/;
+
+        // Check if the room name and username match the regex
+        if (!regex.test(data.room) || !regex.test(data.name)) {
+            alert("Room name and username can only contain letters and numbers.");
+            setRoomNameAndUsername();
+            return;
+        }
+
+        data.room = DOMPurify.sanitize(data.room);
+        data.name = DOMPurify.sanitize(data.name);
+
         setRoomNameAndUsername(data);
     };
 
